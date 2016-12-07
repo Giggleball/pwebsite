6ce23548fbc562d58f4a04545bf1896c505e2588
Giggleball <boldewijn.eli@gmail.com>
2016-12-07 15:48:21 +0100

    cleaned up the repo, fixed footer and made it sassy ;)

--- public/css/script.js
+++
@@ -1,5 +0,0 @@
-// Here you write everything you want to show on the html
- 
-// if you hve multiple scripts/files you want you use, require them all i this one, because this is the one which is linked to the html document
-//let message = require('index.scss')
-//require('.css/index.scss')
